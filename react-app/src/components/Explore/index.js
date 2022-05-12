456440d118f424b3bc704e313163eee2e09c3109
arianna <arianna.n.robinson@gmail.com>
2022-05-12 19:07:34 -0400

    more comments visuals with display #

--- react-app/src/components/Explore/index.js
+++ react-app/src/components/Explore/index.js
@@ -30,10 +30,9 @@ const Explore = () => {
                 <div className="menu feed">
                     {chants.reverse().map(chant => (
                         <div key={chant.id}>
-                            <div>{chant?.user_id}</div>
-                            <div>{chant?.content}</div>
-                            {/* <div>{chant?.remarks}</div> */}
-                            <div>
+                            <div className="chant">{chant?.user_id}</div>
+                            <div className="chant">{chant?.content}</div>
+                            <div className="chant">
                                 {chant?.created_at.split(" ")[0]}{" "}
                                 {chant?.created_at.split(" ")[2]}{" "}
                                 {chant?.created_at.split(" ")[1]},{" "}
@@ -64,18 +63,50 @@ const Explore = () => {
                                 ) > 12 ? 'PM' : 'AM'}
                             </div>
                             <div className='chant-options-container'>
-                                <div>
-                                    <i onClick={() => setToggleComment(toggleComment ? '' : chant?.id)} className="fa-solid fa-comment"></i>
+                                <div className="chant-options">
+                                    <i onClick={() => setToggleComment(toggleComment ? '' : chant?.id)} className="fa-solid fa-comment icon3"></i>
+                                    <div className='num-chants fa-solid'>{chant?.remarks?.length}</div>
                                 </div>
                                 <div>
-                                    <i className="fa-solid fa-fire-flame-curved"></i>
+                                    <i className="fa-solid fa-fire-flame-curved icon3"></i>
                                 </div>
                             </div>
                             {toggleComment !== '' && +toggleComment === +chant?.id && chant?.remarks.map(remark => (
-                                <div key={remark.id}>
-                                    <div>{remark?.content}</div>
-                                    <div>{remark?.user_id}</div>
-                                    <div>{remark?.created_at}</div>
+                                <div className='remark-container' key={remark.id}>
+                                    <div className="remark-details-container">
+                                        <div className="remark">{remark?.user_id}</div>
+                                        <div className="remark" id='remark-created-at'>
+                                            {remark?.created_at.split(" ")[0]}{" "}
+                                            {remark?.created_at.split(" ")[2]}{" "}
+                                            {remark?.created_at.split(" ")[1]},{" "}
+                                            {remark?.created_at.split(" ")[3]} at{" "}
+                                            {Number(
+                                                remark?.created_at
+                                                    .split(" ")[4]
+                                                    .split(":")[0]
+                                            ) <= 12
+                                                ? remark?.created_at
+                                                    .split(" ")[4]
+                                                    .split(":")[0]
+                                                : Number(
+                                                    remark?.created_at
+                                                        .split(" ")[4]
+                                                        .split(":")[0]
+                                                ) - 12}
+                                            :
+                                            {
+                                                remark?.created_at
+                                                    .split(" ")[4]
+                                                    .split(":")[1]
+                                            }
+                                            {Number(
+                                                remark?.created_at
+                                                    .split(" ")[4]
+                                                    .split(":")[0]
+                                            ) > 12 ? 'PM' : 'AM'}
+                                        </div>
+                                    </div>
+                                    <div className="remark">{remark?.content}</div>
                                 </div>
                             ))}
                             <div className='divider'></div>
