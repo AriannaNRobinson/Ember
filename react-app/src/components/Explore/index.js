426a5025b33ca2467557e8d925ca55bb5fffc584
arianna <arianna.n.robinson@gmail.com>
2022-05-12 19:55:29 -0400

    visual changes to explore page

--- react-app/src/components/Explore/index.js
+++ react-app/src/components/Explore/index.js
@@ -29,52 +29,56 @@ const Explore = () => {
                 {/* ALL USERS FEED */}
                 <div className="menu feed">
                     {chants.reverse().map(chant => (
-                        <div key={chant.id}>
-                            <div className="chant">{chant?.user_id}</div>
-                            <div className="chant">{chant?.content}</div>
-                            <div className="chant">
-                                {chant?.created_at.split(" ")[0]}{" "}
-                                {chant?.created_at.split(" ")[2]}{" "}
-                                {chant?.created_at.split(" ")[1]},{" "}
-                                {chant?.created_at.split(" ")[3]} at{" "}
-                                {Number(
-                                    chant?.created_at
-                                        .split(" ")[4]
-                                        .split(":")[0]
-                                ) <= 12
-                                    ? chant?.created_at
-                                        .split(" ")[4]
-                                        .split(":")[0]
-                                    : Number(
+                        <div className='chant-container' key={chant.id}>
+                            <div className='chant-details-container'>
+                                {/* need to change below to user NAME */}
+                                <div className="user1">{chant?.user_id}</div>
+                                <div className="chant" id='chant-created-at'>
+                                    {chant?.created_at.split(" ")[0]}{" "}
+                                    {chant?.created_at.split(" ")[2]}{" "}
+                                    {chant?.created_at.split(" ")[1]},{" "}
+                                    {chant?.created_at.split(" ")[3]} at{" "}
+                                    {Number(
                                         chant?.created_at
                                             .split(" ")[4]
                                             .split(":")[0]
-                                    ) - 12}
-                                :
-                                {
-                                    chant?.created_at
-                                        .split(" ")[4]
-                                        .split(":")[1]
-                                }
-                                {Number(
-                                    chant?.created_at
-                                        .split(" ")[4]
-                                        .split(":")[0]
-                                ) > 12 ? 'PM' : 'AM'}
+                                    ) <= 12
+                                        ? chant?.created_at
+                                            .split(" ")[4]
+                                            .split(":")[0]
+                                        : Number(
+                                            chant?.created_at
+                                                .split(" ")[4]
+                                                .split(":")[0]
+                                        ) - 12}
+                                    :
+                                    {
+                                        chant?.created_at
+                                            .split(" ")[4]
+                                            .split(":")[1]
+                                    }
+                                    {Number(
+                                        chant?.created_at
+                                            .split(" ")[4]
+                                            .split(":")[0]
+                                    ) > 12 ? 'PM' : 'AM'}
+                                </div>
                             </div>
+                            <div className="chant">{chant?.content}</div>
                             <div className='chant-options-container'>
                                 <div className="chant-options">
-                                    <i onClick={() => setToggleComment(toggleComment ? '' : chant?.id)} className="fa-solid fa-comment icon3"></i>
+                                    <i onClick={() => setToggleComment(toggleComment ? '' : chant?.id)} className="fa-solid fa-comment icon4"></i>
                                     <div className='num-chants fa-solid'>{chant?.remarks?.length}</div>
                                 </div>
                                 <div>
-                                    <i className="fa-solid fa-fire-flame-curved icon3"></i>
+                                    <i className="fa-solid fa-fire-flame-curved icon4"></i>
                                 </div>
                             </div>
                             {toggleComment !== '' && +toggleComment === +chant?.id && chant?.remarks.map(remark => (
                                 <div className='remark-container' key={remark.id}>
                                     <div className="remark-details-container">
-                                        <div className="remark">{remark?.user_id}</div>
+                                        {/* need to change below to user NAME */}
+                                        <div className="user2">{remark?.user_id}</div>
                                         <div className="remark" id='remark-created-at'>
                                             {remark?.created_at.split(" ")[0]}{" "}
                                             {remark?.created_at.split(" ")[2]}{" "}
