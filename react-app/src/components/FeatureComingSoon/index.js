b0856023d0e298cad3fe031eada8c26742f9b9c2
arianna <arianna.n.robinson@gmail.com>
2022-05-20 15:11:57 -0400

    feature coming soon page styled

--- react-app/src/components/FeatureComingSoon/index.js
+++ react-app/src/components/FeatureComingSoon/index.js
@@ -1,8 +1,19 @@
 import LeftSideBar from "../LeftSideBar"
 import UsersList from "../Users/UsersList"
 import SuggestedUser from '../SuggestedMembers'
+import './index.css'
+import dragon2 from '../../images/dragoneye1.jpg'
+import { useHistory } from "react-router-dom"
 
 const FeatureComingSoon = () => {
+
+  const history = useHistory()
+
+  const explore = (e) => {
+    e.preventDefault()
+    history.push('/explore-wilds')
+  }
+
   return (
     <div className="page-container">
 
@@ -10,12 +21,16 @@ const FeatureComingSoon = () => {
 
       <div className="main-content-container">
         <div className="menu highest-ele create-chant-box">
-          <div className='user1'>
-            <h2>Stay tuned!</h2>
+          <div className='user1' id='feature-coming'>
+            <h2>Keep an <em>eye</em> out!</h2>
             <h2>This feature will be arriving soon!</h2>
+            <img id='feature-img' src={dragon2} alt='dragon2'></img>
             <h2>In the meantime...</h2>
-            <h2>Explore the wilds!</h2>
+            <h2>Explore the wilds!
+              <button onClick={explore} className="back-explore">Explore!</button>
+              </h2>
           </div>
+          
         </div>
 
         {/* FEATURE COMING SOON PAGE */}
